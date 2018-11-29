94dad3a4f4b202ec27b16eaba6868d5eb7af0288
Necula-Horatiu <necula.horatiu@gmail.com>
2018-11-29 23:28:42 +0200

    Added services and typingDNA js files

---
+++ HowYouType/wwwroot/home/homeController.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('mainApp').controller('homeController', ['dataContext', function (dataContext) {
+        var vm = this;
+        var tdna = new TypingDNA();
+        console.log('aici1');
+
+        vm.show = function () {
+            console.log('aici');
+            console.log(tdna.getTypingPattern({ type: 1, text: vm.input }));
+        };
+
+    }]);
+
+})();
---
+++ HowYouType/wwwroot/services/dataContext.js
@@ -0,0 +1,21 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('mainApp').service('dataContext', ['httpService', function (httpService) {
+        var service = {
+            getData: getData,
+            postData: postData
+        };
+
+        function getData() {
+            return 0;
+        }
+
+        function postData(path, data) {
+            return 0;
+        }
+
+        return service;
+    }]);
+
+})();
